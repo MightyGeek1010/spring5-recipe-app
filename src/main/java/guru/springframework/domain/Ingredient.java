fcf2e682b2d396c04c8d49126e885068da566238
Singhtaravirak Chea <singhtaravirak@gmail.com>
2020-12-21 19:51:17 +0700

    add Ingredient model and its relationship with Recipe

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,32 @@
+package guru.springframework.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Ingredient {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Getter
+	@Setter
+	private Long id;
+	@Getter
+	@Setter
+	private String description;
+	@Getter
+	@Setter
+	private BigDecimal amount;
+	@Getter
+	@Setter
+	@OneToMany
+	private Recipe recipe;
+
+}

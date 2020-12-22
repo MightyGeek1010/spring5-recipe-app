cfd23c9c21fe14e78e706ceb37b579ebbf861208
Singhtaravirak Chea <singhtaravirak@gmail.com>
2020-12-22 21:08:02 +0700

    add repositories

---
+++ src/main/java/guru/springframework/repositories/UnitOfMeasure.java
@@ -0,0 +1,7 @@
+package guru.springframework.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasure extends CrudRepository<UnitOfMeasure, Long> {
+
+}
